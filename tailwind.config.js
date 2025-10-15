0c0396e93bc8c3c02520529edfd666474880f086
clothillde <paulina.twarog@gmail.com>
2025-10-15 08:42:42 +0200

    Add animations in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,ts,scss}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'blink-green': 'blink-green 0.5s ease-in-out',
+        'blink-red': 'blink-red 0.5s ease-in-out',
+        'blink-blue': 'blink-blue 1.5s ease-in-out 3',
+      },
+      keyframes: {
+        'blink-green': { '0%, 100%': { backgroundColor: 'transparent' }, '50%': { backgroundColor: 'rgba(0, 255, 0, 0.3)' } },
+        'blink-red': { '0%, 100%': { backgroundColor: 'transparent' }, '50%': { backgroundColor: 'rgba(255, 0, 0, 0.3)' } },
+        'blink-blue': { '0%, 100%': { backgroundColor: 'transparent' }, '50%': { backgroundColor: 'rgba(0, 0, 255, 0.3)' } },
+      },
+    },
   },
   plugins: [],
-}
-
+};

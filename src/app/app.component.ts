77ff5c48c223ea4d03564fe9950fac1dd9e10ab8
clothillde <paulina.twarog@gmail.com>
2025-10-15 20:07:44 +0200

    Try to get data and display it in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignalRService } from './services/signalr.service';
+import { IStock } from './interfaces/stock.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'stock-table-app';
+export class AppComponent implements OnInit {
+  stocks: IStock[] = [];
+
+  constructor(private signalRService: SignalRService) {}
+
+  ngOnInit(): void {
+    console.log('AppComponent init');
+    this.signalRService.startConnection();
+
+    this.signalRService.stocks$.subscribe(data => {
+      console.log('Angular received stocks:', data);
+      this.stocks = data;
+    });
+  }
 }

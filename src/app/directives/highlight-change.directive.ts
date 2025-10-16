6590f5df815c01b406994bf7e3f96ff1c26c6fe4
clothillde <paulina.twarog@gmail.com>
2025-10-16 13:11:07 +0200

    Directive cleanup

--- src/app/directives/highlight-change.directive.ts
+++ src/app/directives/highlight-change.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 
 @Directive({
   selector: '[highlightChange]',
@@ -8,29 +8,29 @@ export class HighlightChangeDirective implements OnChanges {
   @Input('highlightChange') value!: number | string | null;
   private _prevValue: number | string | null = null;
 
-  constructor(private _el: ElementRef, private _renderer: Renderer2) {}
+  constructor(private _el: ElementRef) {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['value'] && this._prevValue !== null && this.value !== null) {
-      if (typeof this.value === 'number' && typeof this._prevValue === 'number') {
-        if (this.value > this._prevValue) {
-          this._flash('animate-blink-green');
-        } else if (this.value < this._prevValue) {
-          this._flash('animate-blink-red');
-        }
-      } else if (this.value !== this._prevValue) {
-        this._flash('animate-blink-blue');
-      }
+  ngOnChanges(): void {
+    if (this._prevValue === null || this.value === null) {
+      this._prevValue = this.value;
+      return;
     }
+
+    let animationClass: string | null = null;
+
+    if (typeof this.value === 'number' && typeof this._prevValue === 'number') {
+      if (this.value > this._prevValue) animationClass = 'animate-blink-green';
+      else if (this.value < this._prevValue) animationClass = 'animate-blink-red';
+    } else if (this.value !== this._prevValue) {
+      animationClass = 'animate-blink-blue';
+    }
+
+    if (animationClass) {
+      this._el.nativeElement.classList.add(animationClass);
+      const duration = animationClass.includes('blue') ? 4500 : 500;
+      setTimeout(() => this._el.nativeElement.classList.remove(animationClass), duration);
+    }
+
     this._prevValue = this.value;
-  }
-
-  private _flash(animationClass: string) {
-    this._renderer.addClass(this._el.nativeElement, animationClass);
-
-    const duration = animationClass.includes('blue') ? 1500 * 3 : 500;
-    setTimeout(() => {
-      this._renderer.removeClass(this._el.nativeElement, animationClass);
-    }, duration);
   }
 }
